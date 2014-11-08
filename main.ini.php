6851c99092a7f9264560524ed9d720efae1409f4
Oshane Bailey <b4.oshany@gmail.com>
2014-11-08 02:26:26 -0500

    Added author description.

--- main.ini.php
+++ main.ini.php
@@ -22,7 +22,7 @@ if(User::is_login()){
 $books = array(
     array("author"=>"Oshane Bailey", "book_title"=>"Vecni", "genre"=>"Technology", "cover_photo"=>"http://www.technaturals.com/wp-content/uploads/2013/08/Technology.jpg", "ratings"=>4, "date_added"=> (time() - 1032)),
     array("author"=>"Andrelle Thompson", "book_title"=>"Marco", "genre"=>"Literature", "cover_photo"=>"http://c.tadst.com/gfx/600x400/galician-literature-day-spain.jpg?1", "ratings"=>3.5, "date_added"=> (time() - 2232)),
-    array("author"=>"Eric NeedHam", "book_title"=>"Best Arts Practises", "genre"=>"Arts and Recreation", "cover_photo"=>"http://www.designindaba.com/sites/default/files/node/page/23/IMG_3015.jpg", "ratings"=>4.5, "date_added"=> (time() - 32)),
+    array("author"=>"Eric Needham", "book_title"=>"Best Arts Practises", "genre"=>"Arts and Recreation", "cover_photo"=>"http://www.designindaba.com/sites/default/files/node/page/23/IMG_3015.jpg", "ratings"=>4.5, "date_added"=> (time() - 32)),
     array("author"=>"Cloyde McBeth", "book_title"=>"Programming For Dummies", "genre"=>"Computer Science", "cover_photo"=>"http://bobchoat.files.wordpress.com/2014/06/where-is-technology-heading.jpg", "ratings"=>4, "date_added"=> (time() - 12)),
     array("author"=>"Marc Lynch", "book_title"=>"Work Ethics", "genre"=>"Genera Work", "cover_photo"=>"http://artsonthepeninsula.files.wordpress.com/2012/08/we-value-the-arts1.jpg", "ratings"=>2.5, "date_added"=> (time() - 132)),
     array("author"=>"Neil Armstrong", "book_title"=>"Around The Globe", "genre"=>"Geography", "cover_photo"=>"http://sd.keepcalm-o-matic.co.uk/i/keep-calm-and-study-geography-113.png", "ratings"=>3, "date_added"=> (time() - 1932)),
@@ -33,6 +33,19 @@ $books = array(
     array("author"=>"Will Smith", "book_title"=>"Steam Turbines", "genre"=>"Technology", "cover_photo"=>"http://3.bp.blogspot.com/-YFPIfvj7h0M/U6OD1EglnOI/AAAAAAAACdY/VEqw1DlqTy4/s1600/technology44-743413.png", "ratings"=>4, "date_added"=> (time() - 1032)),
     array("author"=>"Aca Cop", "book_title"=>"Islam a Religion or Excuse", "genre"=>"Religion", "cover_photo"=>"http://kenanmalik.files.wordpress.com/2013/08/religion-praying.jpg?w=800", "ratings"=>2, "date_added"=> (time() - 10)),
     array("author"=>"Dean Jones", "book_title"=>"Mastering UI Design", "genre"=>"Arts and Recreation", "cover_photo"=>"http://www.ronnestam.com/wp-content/uploads/2013/03/design-thinking.jpg", "ratings"=>5, "date_added"=> (time() - 1032))
+);
+
+$authors =  array(
+    "Oshane Bailey"=>"Summis fabulas efflorescere. Admodum minim dolor aut lorem. Aut ex summis
+    admodum, o cupidatat imitarentur sed minim ne a sunt occaecat, incurreret sunt
+    dolor iudicem quis, veniam e iudicem ut ipsum, sed cillum ipsum et fabulas o eu
+    anim firmissimum, a quae labore qui proident. Appellat transferrem est voluptate
+    eu excepteur quid cernantur voluptate.",
+    "Eric Needham"=>"Quorum qui se dolor incurreret ea e elit officia coniunctione. Ne lorem de duis
+    ita quo deserunt efflorescere ad voluptate nisi quid non quis et ea dolor
+    laborum praetermissum iis appellat quorum laborum eu cupidatat sunt qui
+    quibusdam arbitrantur e an fugiat instituendarum an elit id iis duis quibusdam.
+    Legam ubi in elit quamquam, nulla expetendis transferrem."
 );
 
 #allow call to static functions
@@ -114,7 +127,7 @@ function genre_view(){
 
 Vecni::set_route("/author/{author}", "author_view");
 function author_view(){
-    global $twig, $books;
+    global $twig, $books, $authors;
     $author = Request::GET("author");
     $author_books = array();
     foreach($books as $book){
@@ -122,11 +135,13 @@ function author_view(){
             array_push($author_books, $book);
         }
     }
+    $description=$authors[$author];
     return $twig->render('book_filter.html',
                   array(
-                    "html_class"=>"book",
+                    "html_class"=>"author",
                     "title"=>"Books by $author",
                     "books"=>$author_books,
+                    "description"=>$description,
                     "menu_active"=>"author/$author"
                   )
               );
